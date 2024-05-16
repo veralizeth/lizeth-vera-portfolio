93f41236b38d03f31e9f3c93a87792d365a43cee
Lizeth Vera <lizethvera@Lizeths-MacBook-Pro.local>
2024-05-15 20:42:24 -0700

    Adding animation to letters and useEfect to set timer

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+
+const Logo = () => ('hello')
+
+
+export default Logo;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,30 +1,54 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom';
 import './index.scss'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 
 
 const Home = () => {
 
-    const [letterClass, setletterClass] = useState('text-animate');
+    const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['l', 'i', 'z', 'e', 't', 'h'];
-    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
+    const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
 
+        // Return a cleanup function that clears the timeout
+        return () => clearTimeout(timer);
+    }, [])
 
     return (
         <div className="container home-page">
             <div className='text-zone'>
-                <h1>Hi, <br /> I'm
-                    <img src={LogoTitle} alt="developer" />
-                    Lizeth vera
+                {/* <h1>Hi, <br /> I'm */}
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
                     <br />
-                    Software Engineer
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+
+                    <img src={LogoTitle} alt="developer" />
+
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
                 </h1>
                 <h2>Full stack Engineer / Java / Podcaster</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo/>
         </div>
     )
 };
